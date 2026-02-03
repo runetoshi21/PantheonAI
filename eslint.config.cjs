77f4008d14db21c8d8871c99d8a1e72576069f2a
runetoshi21 <runetoshi21@users.noreply.github.com>
2026-02-03 22:31:30 +0700

    Set up TypeScript backend scaffold

---
+++ eslint.config.cjs
@@ -0,0 +1,22 @@
+const tseslint = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+
+module.exports = [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tseslint
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }]
+    }
+  }
+];
